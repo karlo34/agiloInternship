a5d7ba99893a34b1978f19277754ccf9a358e179
karlo34 <marasovick1@gmail.com>
2025-07-19 15:53:36 +0200

    Whole shop page including nav and footer are done

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -48,26 +48,26 @@ const Shop = () => {
                         <CollectionSection products={products ?? []} maxItems={4} />
                     )
                 }
-                <h1 className="text-2xl md:text-5xl font-normal sm:font-medium mt-30 mb-10">Shop</h1>
+                <h1 className="text-2xl md:text-5xl font-normal sm:font-medium sm:mt-30 sm:mb-10 mt-15 mb-5">Shop</h1>
 
                 <div className="flex flex-wrap justify-between gap-4 mb-8 text-sm">
                     <div className="flex flex-wrap gap-4">
                         <div className="relative sm:hidden">
-                            <select className="block sm:hidden p-2 border items-center rounded-sm h-[40px] w-[140px] bg-white text-center pr-8 appearance-none border-gray-200">
+                            <select className="block sm:hidden px-2 border items-center rounded-sm h-[33px] w-[73px] bg-white text-center pr-8 appearance-none border-gray-200 text-xs">
                                 <option value="">Filter</option>
-                                <option value="new">Sort by: Newest</option>
-                                <option value="old">Sort by: Oldest</option>
+                                <option value="new">Sort by: Filter</option>
+                                <option value="old">Sort by: Filter</option>
                                 <option value="priceLowHigh">Price: Low to High</option>
                                 <option value="priceHighLow">Price: High to Low</option>
                             </select>
                             {/* Custom plus icon positioned over the select */}
-                            <div className="absolute inset-y-0 right-2 flex items-center pointer-events-none">
-                                <Plus className="h-5 w-5 text-gray-500" />
+                            <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                                <Plus className="h-4 w-4 text-gray-500" />
                             </div>
                         </div>
                         <div className="relative sm:inline-block hidden">
                             <select
-                                className="appearance-none sm:block p-2 border rounded-sm h-[40px] w-[140px] bg-white text-center pr-8 focus:outline-none border-gray-200">
+                                className="appearance-none sm:block p-2 border rounded-sm h-[40px] w-[140px] bg-white text-center pr-8 focus:outline-none border-gray-200 text-base">
                                 <option value="">Collection</option>
                                 <option value="Scandinavian Simplicity">Scandinavian Simplicity</option>
                                 <option value="Modern Luxe">Modern Luxe</option>
@@ -77,7 +77,7 @@ const Shop = () => {
                             <ChevronDown className="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 w-6 h-6" />
                         </div>
                         <div className="relative sm:inline-block hidden">
-                            <select className="appearance-none sm:block p-2 border rounded-sm w-[133px] bg-white text-center pr-8 focus:outline-none border-gray-200">
+                            <select className="appearance-none sm:block p-2 border rounded-sm h-10 w-[133px] bg-white text-center pr-8 focus:outline-none border-gray-200 text-base">
                                 <option value="">Category</option>
                                 <option value="Sofa">Sofa</option>
                                 <option value="Armchair">Armchair</option>
@@ -85,7 +85,7 @@ const Shop = () => {
                             <ChevronDown className="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 w-6 h-6" />
                         </div>
                         <div className="relative sm:inline-block hidden">
-                            <select className="appearance-none sm:block p-2 border rounded-sm w-[99px] bg-white text-center pr-8 focus:outline-none border-gray-200">
+                            <select className="appearance-none sm:block p-2 border rounded-sm h-10 w-[99px] bg-white text-center pr-8 focus:outline-none border-gray-200 text-base">
                                 <option value="">Type</option>
                                 <option value="2-seater">2-Seater</option>
                                 <option value="3-seater">3-Seater</option>
@@ -94,14 +94,14 @@ const Shop = () => {
                         </div>
                     </div>
                     <div className="relative">
-                        <select className="appearance-none sm:block p-2 border rounded-sm bg-white text-center pr-8 focus:outline-none border-gray-200">
+                        <select className="appearance-none sm:block p-2 border rounded-sm sm:h-10 sm:w-[117px] h-[33px] w-[84px]  bg-white text-center pr-8 focus:outline-none border-gray-200 sm:text-base text-xs">
                             <option value="">Sort by</option>
                             <option value="new">Sort by: Newest</option>
                             <option value="old">Sort by: Oldest</option>
                             <option value="priceLowHigh">Price: Low to High</option>
                             <option value="priceHighLow">Price: High to Low</option>
                         </select>
-                        <ChevronDown className="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 w-6 h-6" />
+                        <ChevronDown className="pointer-events-none absolute top-1/2 right-2 -translate-y-1/2 text-gray-500 sm:w-6 sm:h-6 w-5 h-5" />
                     </div>
                 </div>
                 {
